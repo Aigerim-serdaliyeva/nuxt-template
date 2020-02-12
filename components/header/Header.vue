6c87bfc0ce1e6bdc9ed80d082bc636deab2d363d
Айгерим Сердалиева <aigerim.serdaliyeva@mail.ru>
2020-02-12 11:27:28 +0600

    deleted

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,30 +1,9 @@
-<template>
-    <div class="header w-full font-roboto z-10 py-5px sm:py-0 fixed">
-        <div class="container">
-            <div class="w-11/12 mx-auto flex items-center sm:justify-between">
-                <nuxt-link to="/">
-                    <img src="@/assets/img/logo.svg" alt="" />
-                </nuxt-link>
-                <MainMenu class="hidden sm:block" />
-            </div>
-        </div>
-    </div>
-</template>
+<template> <div></div> </template>
 
 <script>
-import MainMenu from './MainMenu.vue';
 export default {
-    components: { MainMenu }
+    components: {}
 };
 </script>
 
-<style lang="scss" scoped>
-.header {
-    background: radial-gradient(
-        7683.68% 132444.76% at 50% 49.99%,
-        #ffffff 0%,
-        #122841 100%
-    );
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-}
-</style>
+<style lang="scss" scoped></style>
--- components/header/HeaderMobile.vue
+++ components/header/HeaderMobile.vue
@@ -2,10 +2,10 @@
     <div>
         <div class="container header fixed z-10 py-5px">
             <div class="absolute top-22px left-17px" @click="openMenu()">
-                <img src="../../assets/img/open.svg" />
+                <!-- <img src="../../assets/img/open.svg" /> -->
             </div>
             <nuxt-link to="/" class="text-center mx-auto block">
-                <img src="@/assets/img/logo.svg" class="mx-auto" />
+                <!-- <img src="@/assets/img/logo.svg" class="mx-auto" /> -->
             </nuxt-link>
         </div>
         <div
@@ -13,18 +13,17 @@
             :class="{ active: mobileMenuState }"
         >
             <div @click="closeMenu()">
-                <img
+                <!-- <img
                     src="../../assets/img/exit.svg"
                     class="absolute top-17px left-17px"
-                />
+                /> -->
             </div>
 
             <nuxt-link to="/" class="max-w-140px mx-auto">
-                <img src="@/assets/img/logo-white.svg" alt="" />
+                <!-- <img src="@/assets/img/logo-white.svg" alt="" /> -->
             </nuxt-link>
             <div class="flex-grow"></div>
             <MainMenu @onSelectItem="closeMenu" />
-            <SocialIcons />
             <p class="text-gray-1 text-center text-12px opacity-30 mt-25px">
                 All Rights Reserved 2019 «Muratov Partners
             </p>
@@ -34,9 +33,8 @@
 
 <script>
 import MainMenu from './MainMenu.vue';
-import SocialIcons from './SocialIcons.vue';
 export default {
-    components: { SocialIcons, MainMenu },
+    components: { MainMenu },
     data() {
         return {
             mobileMenuState: false
@@ -53,23 +51,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.header-mobile {
-    background-color: #f9fcfc;
-    background-image: url('../../assets/img/header-mob-back.png');
-    left: -100%;
-    transition: 0.4s left ease-in;
-    &.active {
-        left: 0;
-    }
-}
-.header {
-    background: radial-gradient(
-        7683.68% 132444.76% at 50% 49.99%,
-        #ffffff 0%,
-        #122841 100%
-    );
-    opacity: 0.9;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-}
-</style>
+<style lang="scss" scoped></style>
